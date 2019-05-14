73087be00beb6120a0463421aca4df5ea9332b44
Duan Yupeng <fengdurant@163.com>
2019-05-14 16:30:50 +0800

    ercigongshui pcindex page repair bar axisLabel style

--- studydemo/bootstrap/waterIndex.js
+++ studydemo/bootstrap/waterIndex.js
@@ -1,5 +1,5 @@
 /** 设备保养*/
-var equip = echarts.init(document.getElementById('equip'));
+/* var equip = echarts.init(document.getElementById('equip'));
 var option = {
   tooltip: {
     trigger: 'item',
@@ -41,3 +41,46 @@ var option = {
 };
 
 equip.setOption(option);
+ */
+
+var vendor = echarts.init(document.getElementById('vendor-statis'));
+var option = {
+  color: ['#3398DB'],
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: { // 坐标轴指示器，坐标轴触发有效
+      type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+    }
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [{
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    axisTick: {
+      alignWithLabel: true
+    },
+    axisLabel: {
+      show: true,
+      textStyle: {
+        color: '#c3dbff', //更改坐标轴文字颜色
+        fontSize: 14 //更改坐标轴文字大小
+      }
+    },
+  }],
+  yAxis: [{
+    type: 'value'
+  }],
+  series: [{
+    name: '直接访问',
+    type: 'bar',
+    barWidth: '60%',
+    data: [10, 52, 200, 334, 390, 330, 220]
+  }]
+};
+
+vendor.setOption(option);

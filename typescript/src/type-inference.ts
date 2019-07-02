0667da9a3ad9edb92495150ad811d4b2db921fec
Duan Yupeng <fengdurant@163.com>
2019-07-02 18:06:14 +0800

    add union-type

---
+++ typescript/src/type-inference.ts
@@ -0,0 +1,4 @@
+let myFavoriteNumber='seven'
+//myFavoriteNumber=7
+
+//error TS2322: Type 'number' is not assignable to type 'string'.
---
+++ typescript/src/union-types.ts
@@ -0,0 +1,9 @@
+let myFavoriteNum: string|number;
+myFavoriteNum='seven'
+myFavoriteNum=7
+
+let myFavoriteNum2: string|number;
+//myFavoriteNum2=true
+
+// error TS2322: Type 'boolean' is not assignable to type 'string | number'.
+//   Type 'boolean' is not assignable to type 'number'.

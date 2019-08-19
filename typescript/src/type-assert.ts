759360ebba27efca52e1e57123ec35303ce437cb
Duan Yupeng <fengdurant@163.com>
2019-08-19 20:18:39 +0800

    add code

---
+++ typescript/src/type-assert.ts
@@ -0,0 +1,11 @@
+//function getLength(something: string | number): number {
+  // if (something.length) {
+  //   return something.length;
+  // }
+  // else {
+  //   return something.toString().length
+  // }
+//}
+
+// index.ts(2,19): error TS2339: Property 'length' does not exist on type 'string | number'.
+//   Property 'length' does not exist on type 'number'.

284e14f7afd27f7f79aadf86f289de08bcb765d8
lovekyrie <fengdurant@163.com>
2020-11-23 22:30:47 +0800

    增加算法文件夹

---
+++ algorithm/链表/单链表/doubleLinked.js
@@ -0,0 +1,15 @@
+function Node(value) {
+  this.data = value
+  this.previous = null
+  this.next = null
+}
+
+function DoublyList() {
+  this._length = 0;
+  this.head = null
+  this.tail = null
+}
+
+DoublyList.prototype.add = function (value) {
+
+}

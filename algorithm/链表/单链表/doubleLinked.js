c0cfd2461414fa859d8e631492f6f03fa016299b
duanyupeng <fengdurant@163.com>
2020-11-24 08:48:08 +0800

    增加方法

--- algorithm/链表/单链表/doubleLinked.js
+++ algorithm/链表/单链表/doubleLinked.js
@@ -10,6 +10,43 @@ function DoublyList() {
   this.tail = null
 }
 
+/**
+ * add
+ * @param {*} value 
+ */
 DoublyList.prototype.add = function (value) {
+  const node = new Node(value)
 
+  if (this._length) {
+    this.tail.next = node
+    node.previous = this.tail
+    this.tail = node
+  } else {
+    this.head = node
+    this.tail = node
+  }
+  this._length++;
+  return node
+}
+
+DoublyList.prototype.searchNodeAt = function (position) {
+  let currentNode = this.head,
+    length = this._length,
+    count = 1,
+    message = {
+      failure: 'Failure: non-existent node in this list'
+    }
+
+  //1st use-case: an invalid position
+  if (length === 0 || position < 1 || position > length) {
+    return new Error(message.failure)
+  }
+
+  //2st use-case: a valid position
+  while (count < position) {
+    currentNode = currentNode.next
+    count++
+  }
+
+  return currentNode;
 }

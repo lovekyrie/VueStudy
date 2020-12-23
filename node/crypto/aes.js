b37b3c5de6df4172a71610ec93169842e476f7a5
duanyupeng <fengdurant@163.com>
2020-12-23 17:31:23 +0800

    增加加解密模块

--- node/crypto/aes.js
+++ node/crypto/aes.js
@@ -2,33 +2,54 @@
 
 const crypto = require('crypto')
 
+const algorithm = 'aes-256-cbc'
+// define key
+const key = crypto.randomBytes(32)
+// define iv
+const iv = crypto.randomBytes(16)
 /**
  * key是用于加密的密钥； iv参数可选， 用于指定加密时所用的向量。 注意这里的密钥必须是8 / 16 / 32 位，
  * 如果加密算法是128， 则对应的密钥是16位， 如果加密算法是256， 则对应的密钥是32位。 
  */
-function aesEncrypt(data, key, iv) {
+function aesEncrypt(data) {
 
-  const cipher = crypto.createCipheriv('aes-128-gcm', key, iv)
-  let crypted = cipher.update(data, 'binary', 'base64')
-  crypted += cipher.final('base64')
-  return crypted
+  //Creating Cipheriv with its parameter
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
+
+  //updating data
+  let crypted = cipher.update(data)
+
+  //using concatenation
+  crypted = Buffer.concat([crypted, cipher.final()])
+
+  //returning iv and encrypted data
+  return {
+    iv: iv.toString('hex'),
+    encryptedData: crypted.toString('hex')
+  }
 }
 
+//A decrypt function
+function aesDecrypt(data) {
 
-function aesDecrypt(encrypted, key, iv) {
+  let iv = Buffer.from(data.iv, 'hex')
+  let encryptedText = Buffer.from(data.encryptedData, 'hex')
 
-  const decipher = crypto.createDecipheriv('aes-128-gcm', key, iv)
-  let decrypted = decipher.update(encrypted, 'binary', 'utf8')
-  decrypted += decipher.final('utf8')
-  return decrypted
+  //Creating decipher
+  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
+
+  //updating encrypted data
+  let decrypted = decipher.update(encryptedText)
+  decrypted = Buffer.concat([decrypted, decipher.final()])
+
+  //returns data after decryption
+  return decrypted.toString()
 }
 
 const data = 'Hello, this is a secret message!'
-const key = 'password12345678'
-const iv = '12345678password'
-const encrypted = aesEncrypt(data, key, iv)
-const decrypted = aesDecrypt(encrypted, key, iv)
+const encrypted = aesEncrypt(data)
+const decrypted = aesDecrypt(encrypted)
 
 console.log('Plain text: ' + data);
-console.log('Encrypted text: ' + encrypted);
-console.log('Decrypted text: ' + decrypted);
+console.log(encrypted);
+console.log(decrypted)

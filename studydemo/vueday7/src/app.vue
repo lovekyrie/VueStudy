508e937d836dc873b0f403e3924b7e1e2a09ec58
duanyupeng <fengdurant@163.com>
2021-02-01 09:45:59 +0800

    增加router-view

--- studydemo/vueday7/src/app.vue
+++ studydemo/vueday7/src/app.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    {{msg}}  
+    {{ msg }}
     <!-- <router-view></router-view> -->
     <button @click="show">notify</button>
-  </div>  
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      msg:'hello'
-    }
+      msg: "hello",
+    };
   },
-  methods:{
-    show(){
-      this.$notify('我太丑',{delay:1000})
-    }
-  }
-}
+  methods: {
+    show() {
+      this.$notify("我太丑", { delay: 1000 });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
 
